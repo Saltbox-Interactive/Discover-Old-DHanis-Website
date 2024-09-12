e98abdf5bd7d2d3ff321a93c14af4471281f6289
Terry Ju <t.juottawa@gmail.com>
2024-09-12 01:43:49 -0400

    added styling for home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,18 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import About from './about';
 import Team from './team';
 import Gallery from './gallery';
-
-
+import Image from "next/image";
 
 export default function Home() {
   return (
     // This will be overarching container
-    <div >
-      <div className={styles.container}> 
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    
+    <div className={styles.pageFormatting}>
+      <img className={styles.backgroundImg} src="/assets/homeWallpaper.jpg"/>
 
       <header>
         <div className={styles.header}>
@@ -24,22 +20,28 @@ export default function Home() {
             <h1>Old D'Hanis</h1>
           </div>
 
-          <div className={styles.menuItems}>
-            <Button as="a" variant="primary">About</Button>
-            <Button as="a" variant="primary">Team</Button>
-            <Button as="a" variant="primary">Gallery</Button>
+          <div className={styles.menuItemsContainer}>
+            <Button variant="light" className={styles.menuItems} as="a">About</Button>
+            <Button variant="light" className={styles.menuItems} as="a">Team</Button>
+            <Button variant="light" className={styles.menuItems} as="a">Gallery</Button>
           </div>
         </div>
       </header>
 
-      <main>
-        <h1 className={styles.title}>
-          <a>Welcome to Old D'Hanis</a>
-        </h1>
-        <div className={styles.downLoad}>
-          <Button>Download on steam</Button>
-        </div>
-      </main>
+      <div className={styles.container}> 
+        <Head>
+          <title>Explore Old D'Hanis</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+         
+          <h1 className={styles.title}>
+            <a>Welcome to Old D'Hanis</a>
+          </h1>
+          <div className={styles.download}>
+            <Button variant="light" size='lg'> Download on steam </Button>
+          </div>
+        </main>
       </div>
 
       <About></About>
