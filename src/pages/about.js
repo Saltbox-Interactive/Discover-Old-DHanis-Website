23cb10531fecdbbdf99d02a60a1ea14bda0f8889
Terry Ju <t.juottawa@gmail.com>
2024-09-18 00:28:30 -0400

    about page in progress

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,21 @@ import styles from '../styles/about.module.scss';
 export default function About(){
     return ( 
         <div className={styles.container}>
-            <h1 className={styles.title}> This will be the about page</h1>
+            <div className={styles.aboutContainer}>
+                <h1 className={styles.title}> About Old D'Hanis</h1>
+                <div className={styles.aboutText}>
+                    <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                    </p>
+                </div>
+            </div>
+           
+            <div className={styles.aboutImage}>
+                
+            </div>
         </div>
     )
 }
