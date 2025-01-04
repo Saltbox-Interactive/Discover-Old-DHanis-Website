25940e353b4eb2eb3b1fb644c4cd4ac0c6b6d4b8
michaelsalton <msalton6@gmail.com>
2025-01-04 13:50:57 -0500

    removed pngs from gitignore and added images bacck

--- src/pages/team.js
+++ src/pages/team.js
@@ -7,35 +7,35 @@ export default function Team(){
             <h1 className={styles.header}> The Team </h1>
             <div className={styles.underline}></div>
             <InfoBox
-                imageSrc='/assets/profile-pic-default.jpg'
+                imageSrc='/assets/dod-logo.png'
                 altText='profile pic'
                 name='Dr. Patricia Markert'
                 role='Supervisor, Researcher, Professor'
                 text='Text goes here'
             />
             <InfoBox
-                imageSrc='/assets/profile-pic-default.jpg'
+                imageSrc='/assets/dod-logo.png'
                 altText='profile pic'
                 name='Michael Salton'
                 role='Programming, Enviroment Design'
                 text='Text goes here'
             />
             <InfoBox
-                imageSrc='/assets/profile-pic-default.jpg'
+                imageSrc='/assets/dod-logo.png'
                 altText='profile pic'
                 name='Emily Grant'
                 role='Art, Modelling'
                 text='Text goes here'
             />
             <InfoBox
-                imageSrc='/assets/profile-pic-default.jpg'
+                imageSrc='/assets/joyce.png'
                 altText='profile pic'
                 name='Hiu Lee Joyce'
                 role='Music, Sound'
                 text='Text goes here'
             />
             <InfoBox
-                imageSrc='/assets/profile-pic-default.jpg'
+                imageSrc='/assets/dod-logo.png'
                 altText='profile pic'
                 name='Terry Yu'
                 role='Web'
