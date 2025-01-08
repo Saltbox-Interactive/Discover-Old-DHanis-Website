b79374fc86f7fe2ae4ca5d58c66cf50761aec91a
Terry Ju <t.juottawa@gmail.com>
2025-01-08 09:59:59 -0500

    test path

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import styles from '../styles/gallery.module.scss';
 
 const images = [
-  { src: '../../public/assets/dod-screenshot-1.png', alt: 'Gallery image 1' },
+  { src: 'https://github.com/Saltbox-Interactive/Discover-Old-DHanis-Website/blob/main/public/assets/background_pic.jpg', alt: 'Gallery image 1' },
   { src: './assets/dod-screenshot-2.png', alt: 'Gallery image 2' },
   { src: './assets/dod-screenshot-3.png', alt: 'Gallery image 3' },
   { src: "/assets/dod-screenshot-4.png", alt: 'Gallery image 4' },
